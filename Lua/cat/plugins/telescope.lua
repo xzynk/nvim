f9c00f36f868794fad37aa011b09dd6b883bb0d1
Anthony Sotomayor <anthonysv@gmail.com>
2024-12-11 16:41:31 -0500

    Add Readme
    Añadido el README y mejoras de formato para telescope.lua

--- Lua/cat/plugins/telescope.lua
+++ Lua/cat/plugins/telescope.lua
@@ -1,33 +1,33 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.8",
-	dependencies = { "nvim-lua/plenary.nvim", { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
-	config = function()
-		local telescope = require("telescope")
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim", { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
+    config = function()
+        local telescope = require("telescope")
 
-		telescope.setup({
-			defaults = {
-				prompt_prefix = "   ",
-				selection_caret = " ",
-				entry_prefix = " ",
-				sorting_strategy = "ascending",
-				layout_config = {
-					horizontal = {
-						prompt_position = "top",
-						preview_width = 0.55,
-					},
-					width = 0.87,
-					height = 0.80,
-				},
-				mappings = {
-					n = { ["q"] = require("telescope.actions").close },
-				},
-			},
-		})
+        telescope.setup({
+            defaults = {
+                prompt_prefix = "   ",
+                selection_caret = " ",
+                entry_prefix = " ",
+                sorting_strategy = "ascending",
+                layout_config = {
+                    horizontal = {
+                        prompt_position = "top",
+                        preview_width = 0.55,
+                    },
+                    width = 0.87,
+                    height = 0.80,
+                },
+                mappings = {
+                    n = { ["q"] = require("telescope.actions").close },
+                },
+            },
+        })
 
-		telescope.load_extension("fzf")
-		telescope.load_extension("noice")
-	end,
+        telescope.load_extension("fzf")
+        telescope.load_extension("noice")
+    end,
 
-	extensions = {},
+    extensions = {},
 }
---
+++ README.md
@@ -0,0 +1,27 @@
+# NVIM CONFIGURACION
+Esta es la primera vez que hago mi propia configuracion, he usado multiples referencias encontradas en la web.
+
+Requiere para funcionar ciertas dependencias de los propios plugins
+
+## Instalar
+
+**Brew**
+Recomiendo instalar brew para instalar las dependecias
+En tu terminal copia lo siguiente:
+
+```shell
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+```
+**npm**
+Esto es para que Mason pueda funcionar, de nuevo, en tu consola copia lo siguiente:
+```shell
+brew install node
+```
+
+**riggrep**
+Para que funcione telescope live grep instalamos lo siguiente:
+```shell
+brew install ripgrep
+```
+
+Eso seria todo. Cualquier sugerencia por favor hazmelo llegar.
